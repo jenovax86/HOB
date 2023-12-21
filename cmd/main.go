1eb113da50fbb0ce5fcae2d3cd53024536968068
jenovax86 <shruzvn@gmail.com>
2023-12-22 02:37:04 +0330

    feat: create simple hob game

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
 
-func main(){
+func hobGame() {
+	var count = 0
+	for count < 50 {
+		count++
+		if count%5 != 0 {
+			fmt.Println(count)
+		} else {
+			fmt.Println("HOB")
+		}
+		time.Sleep(time.Second)
+	}
+}
 
+func main() {
+	hobGame()
 }
